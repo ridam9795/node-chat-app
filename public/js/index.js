840e6a99aac981e39fe4985ffcb6a5fba578be40
rid <ridamnagar11@gmail.com>
2018-08-15 19:53:21 +0530

    NewMessage and CreateMessage event are added

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+var socket=io();
+          socket.on('connect',()=>{
+              console.log('connected to the server');
+              socket.emit('createEmail',{
+                  to:"rishabh@gmail.com",
+                  text:"fine! how about you"
+              });
+              socket.emit('createMessage',{
+                from:'Ridam',
+                text:'hor r u'
+            })
+
+          })
+         
+          socket.on('disconnect',()=>{
+              console.log('disconnected from server');
+          });
+          socket.on('newEmail',function(email){
+              console.log("New Email",email);
+          });
+          socket.on('newMessage',function(message){
+              console.log("Message ",message);
+          })
--- server/server.js
+++ server/server.js
@@ -13,6 +13,22 @@ io.on('connection',(socket)=>{
   console.log("New User Connected");
   socket.on('disconnect',()=>{
     console.log('disconnected from client');
+  });
+  socket.emit('newEmail',{
+    from:"rishabh2gmail.com",
+    text:"how are u?",
+    complatedAt:133
+  });
+  socket.on('createEmail',(createdEmail)=>{
+   console.log("created Email",createdEmail);
+  });
+  socket.emit('newMessage',{
+    from:'Servesh',
+    text:"hey",
+    complatedAt:233
+  });
+  socket.on('createMessage',(createMessage)=>{
+    console.log("Create Message",createMessage);
   })
 })
 server.listen(port, () => {
