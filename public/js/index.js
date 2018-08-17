156c00102c1cfd8035b006a51fe58f249f49184e
rid <ridamnagar11@gmail.com>
2018-08-17 10:54:53 +0530

    socket.broadcast.emit ,socket.emit,io.emit are added to my caht app and is working great

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 var socket=io();
           socket.on('connect',()=>{
               console.log('connected to the server');
-              socket.emit('createEmail',{
-                  to:"rishabh@gmail.com",
-                  text:"fine! how about you"
-              });
-              socket.emit('createMessage',{
-                from:'Ridam',
-                text:'hor r u'
-            })
+            //   socket.emit('createEmail',{
+            //       to:"rishabh@gmail.com",
+            //       text:"fine! how about you"
+            //   });
+            //   socket.emit('createMessage',{
+            //     from:'Ridam',
+            //     text:'hor r u'
+            // })
 
           })
          
--- server/server.js
+++ server/server.js
@@ -10,25 +10,39 @@ app.use(express.static(publicPath));
 var server=http.createServer(app);
 var io=socketIO(server);
 io.on('connection',(socket)=>{
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:"Welcome to the chat app"
+  });
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:"New user joined"
+  })
   console.log("New User Connected");
   socket.on('disconnect',()=>{
     console.log('disconnected from client');
   });
-  socket.emit('newEmail',{
-    from:"rishabh2gmail.com",
-    text:"how are u?",
-    complatedAt:133
-  });
-  socket.on('createEmail',(createdEmail)=>{
-   console.log("created Email",createdEmail);
-  });
-  socket.emit('newMessage',{
-    from:'Servesh',
-    text:"hey",
-    complatedAt:233
-  });
+  // socket.emit('newEmail',{
+  //   from:"rishabh2gmail.com",
+  //   text:"how are u?",
+  //   complatedAt:133
+  // });
+  // socket.on('createEmail',(createdEmail)=>{
+  //  console.log("created Email",createdEmail);
+  // });
+  // socket.emit('newMessage',{
+  //   from:'Servesh',
+  //   text:"hey",
+  //   complatedAt:233
+  // });
   socket.on('createMessage',(createMessage)=>{
     console.log("Create Message",createMessage);
+    socket.broadcast.emit('newMessage',{
+      from:createMessage.from,
+      text:createMessage.text,
+      complatedAt:233
+    });
+  
   })
 })
 server.listen(port, () => {
